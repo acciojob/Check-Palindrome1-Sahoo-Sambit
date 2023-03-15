13eb00b44b707e4f5b151119080b803f30dc2935
Sambit_Sahoo <93509732+Sahoo-Sambit@users.noreply.github.com>
2023-03-15 17:03:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // complete the given function
 
 function palindrome(str){
-
+	str=str.toLowerCase();
+	var rev=str.split("").reverse().join("");
+	return str===rev;
 }
 module.exports = palindrome
